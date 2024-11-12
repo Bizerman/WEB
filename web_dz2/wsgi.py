c7a1d97ad4be1165fe01a6a1901d4d2b3197aaeb
Bizarreman <lesha-utenkov@rambler.ru>
2024-11-12 22:37:02 +0300

    add

--- web_dz2/wsgi.py
+++ web_dz2/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for web_dz1 project.
+WSGI config for web_dz22 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_dz1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_dz22.settings')
 
 application = get_wsgi_application()
