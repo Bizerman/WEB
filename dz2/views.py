ca7a8e16272d2b07e1bad421602d24e7ce6d2e7c
Bizarreman <lesha-utenkov@rambler.ru>
2024-11-13 00:14:45 +0300

    add

--- dz2/views.py
+++ dz2/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
 
-
+tags = [{'name': 'python',
+         'theme': 'btn-primary',
+         },
+        {'name': 'MySQL',
+         'theme': 'btn-secondary',
+         },
+        {'name': 'Sberbank',
+         'theme': 'btn-success',
+         },
+        {'name': 'Opera',
+         'theme': 'btn-danger',
+         },
+        {'name': 'Django',
+         'theme': 'btn-warning',
+         },
+        {'name': 'Vk',
+        'theme': 'btn-info',
+        },
+        ]
 def render_questions_list_page(request):
-    return render(request, 'index.html', {'auth': True})
+    return render(request, 'index.html', {'auth': True, 'tags':tags, 'tag': None, 'sort': None})
+def render_hot_questions_page(request):
+    return render(request, 'index.html', {'auth': True, 'tags':tags, 'tag': None, 'sort': 'Hot'})
+def render_questions_with_tag_page(request, tag):
+    return render(request, 'index.html', {'auth': True, 'tags': tags, 'tag': tag})
 def render_ask_page(request):
     return render(request, 'ask.html')
 def render_question_page(request):
@@ -12,4 +34,4 @@ def render_login_page(request):
 def render_signup_page(request):
     return render(request, 'signup.html')
 def logout(request):
-    return render(request, 'index.html', {'auth': False})
+    return render(request, 'index.html', {'auth': False, 'tags':tags, 'tag': None})
--- web_dz2/urls.py
+++ web_dz2/urls.py
@@ -20,6 +20,8 @@ from dz2 import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.render_questions_list_page, name='list_page_url'),
+    path('/hot', views.render_hot_questions_page, name='list_hot_questions_page'),
+    path('/tag/<str:tag>', views.render_questions_with_tag_page, name='list_tag_url'),
     path('new_question/', views.render_ask_page, name='ask_page_url'),
     path('question/', views.render_question_page, name='question_page_url'),
     path('login/',views.render_login_page,name='login_page_url'),
