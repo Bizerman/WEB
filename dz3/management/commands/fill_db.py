33775e86379f78ebedadc943bab315e7c1c7f1fa
Bizarreman <lesha-utenkov@rambler.ru>
2024-11-14 16:33:31 +0300

    add

--- dz3/management/commands/fill_db.py
+++ dz3/management/commands/fill_db.py
@@ -1,50 +1,56 @@
 from django.core.management.base import BaseCommand
 from django.utils.crypto import get_random_string
-from dz3.models import Profile, Question, Answer, Tag, QuestionLike
+from dz3.models import Profile, Question, Answer, Tag, QuestionLike,AnswerLike  # Импортируйте ваши модели
 import random
 
 class Command(BaseCommand):
-
     def add_arguments(self, parser):
         parser.add_argument('ratio', type=int, help='The ratio for filling the database')
 
-    def handle(self, *args, **options):
-        ratio = options['ratio']
+    def handle(self, *args, **kwargs):
+        ratio = kwargs['ratio']
 
-        users = []
-        for _ in range(ratio):
-            user = Profile.objects.create(username=get_random_string(8), email=f'user{_}@example.com')
-            users.append(user)
+        users = [
+            Profile(username=get_random_string(8), email=f'user{_}@example.com') for _ in range(ratio)
+        ]
+        Profile.objects.bulk_create(users)
 
-        questions = []
-        for _ in range(ratio * 10):
-            question = Question.objects.create(
+        questions = [
+            Question(
                 question=get_random_string(12),
                 description=get_random_string(50),
-                author=random.choice(users)
-            )
-            questions.append(question)
+                author=random.choice(users)  #
+            ) for _ in range(ratio * 10)
+        ]
+        Question.objects.bulk_create(questions)
 
-        answers = []
-        for _ in range(ratio * 100):
-            answer = Answer.objects.create(
+        answers = [
+            Answer(
                 content=get_random_string(30),
                 question=random.choice(questions),
                 author=random.choice(users)
-            )
-            answers.append(answer)
+            ) for _ in range(ratio * 100)
+        ]
+        Answer.objects.bulk_create(answers)
 
-        tags = []
-        for _ in range(ratio):
-            tag = Tag.objects.create(name=get_random_string(5))
-            tags.append(tag)
+        tags = [
+            Tag(name=get_random_string(5)) for _ in range(ratio)
+        ]
+        Tag.objects.bulk_create(tags)
 
-        likes = []
-        for _ in range(ratio * 200):
-            like = QuestionLike.objects.create(
+        qlikes = [
+            QuestionLike(
                 user=random.choice(users),
                 question=random.choice(questions)
-            )
-            likes.append(like)
+            ) for _ in range(ratio * 200)
+        ]
+        QuestionLike.objects.bulk_create(qlikes)
+        alikes = [
+            AnswerLike(
+                user=random.choice(users),
+                question=random.choice(questions)
+            ) for _ in range(ratio * 200)
+        ]
+        QuestionLike.objects.bulk_create(alikes)
 
-        self.stdout.write(self.style.SUCCESS('Успешно добавленно в БД!'))
+        self.stdout.write(self.style.SUCCESS('Успешное добавление в БД!'))
